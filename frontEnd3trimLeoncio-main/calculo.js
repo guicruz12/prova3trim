18756c7162708f026a9b8e1bfae83f88377c2c6c
guicruz12 <130380751+guicruz12@users.noreply.github.com>
2023-10-23 08:55:20 -0300

    Add files via upload

---
+++ frontEnd3trimLeoncio-main/calculo.js
@@ -0,0 +1,16 @@
+
+
+function Calcular(){
+
+    let idadeCanina = prompt("Qual a idade do cachorrinho?");
+    let calculo = idadeCanina * 7;
+console.log(calculo)
+
+
+    //BRO ESTOU PERDIDO NÃO TA FAZENDO SOL, VOCÊ PODE ME AJUDAR A TERMINAR ESSA CALCULADORA PARA MEU FILHO??
+    //POR FAVOR BRO TERMINE PARA MIM GANG GANG GANG 777 FERNANDX CLOATHING
+
+    alert("a idade do seu cachorro" +calculo);
+
+
+}
